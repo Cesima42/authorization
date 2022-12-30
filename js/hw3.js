26e74b159c6b6feb1fc8d9488ce4e1e8bdeecd28
Cesima42 <taileken05@gmail.com>
2022-12-30 11:03:45 +0600

    authorization
    authorization

---
+++ js/hw3.js
@@ -0,0 +1,14 @@
+function func() {
+    var result;
+    var name = (document.getElementById("textInput").value); 
+        while (name.length < 4){
+            name = prompt('Вы некорректно вывели имя, попробуйте снова')
+        }   
+    var name = (document.getElementById("valueInput").value);    
+    var name = (document.getElementById("select").value);    
+
+
+    document.getElementById("result").window.location.href = "http://127.0.0.1:5500/project1.html/calculator/js.html";
+
+}
+
